09a1ae4c30fd6741a3d77542658221716d321347
Olga Dobrova <dobrova_o_g@mail.ru>
2023-09-26 23:36:02 +0300

    Это репозиторий для webpack

---
+++ dist/main.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";console.log("one module"),console.log("webpack"),console.log("two module")})();
---
+++ src/modules/one.js
@@ -0,0 +1,6 @@
+const oneModule = () => {
+  const one = "one module";
+  console.log(one);
+  console.log("webpack");
+};
+export default oneModule;
